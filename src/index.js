ba62c33a4b1e6f22c2bde36cf1b42fe5101e6cd1
Tatsiana1707 <tannysha1987@mail.ru>
2021-08-08 21:33:43 +0300

    test 2

--- src/index.js
+++ src/index.js
@@ -13,24 +13,31 @@ module.exports = function toReadable (num) {
     var numString = num.toString();
   
     
-    if (num === 0) return 'zero';
+    if (num == 0) return 'zero';
    
    
     if (num < 20) {
       return ones[num];
     }
   
-    if (numString.length === 2) {
+    if (numString.length == 2) {
+      if (numString[0] !== '0' && numString[1] === '0')
+      return tens[numString[0]];
+      else
       return tens[numString[0]] + ' ' + ones[numString[1]];
     }
   
      if (numString.length == 3) {
-      if (numString[1] === '0' && numString[2] === '0')
+      if (numString[1] == '0' && numString[2] == '0')
         return ones[numString[0]] + ' hundred';
-     if (numString[1] === '1' && numString[2] === '0')
+      if (numString[1] === '0' && numString[2] !== '0')
+        return ones[numString[0]] + ' hundred' + ' ' + ones[numString[2]];
+      if (numString[1] !== '0' && numString[2] === '0')
+        return ones[numString[0]] + ' hundred' + ' ' + tens[numString[1]];
+      if (numString[1] === '1' && numString[2] === '0')
         return ones[numString[0]] + ' hundred ' + unit[numString[1]];
-     if (numString[1] === '1' && numString[2] !== '0')
-        return ones[numString[0]] + ' hundred ' + units[numString[2]];
+      if (numString[1] == '1' && numString[2] !== '0')
+       return ones[numString[0]] + ' hundred ' + units[numString[2]];
       else
          return ones[numString[0]] + ' hundred ' + tens[numString[1]] + ' ' + ones[numString[2]];
     }}
